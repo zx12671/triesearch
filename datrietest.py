8aaacfc0c4e47d13824800520cba1badf7e8b00e
zx12671 <113357510@qq.com>
2022-06-10 01:30:20 +0800

    test on marisa_trie

--- datrietest.py
+++ datrietest.py
@@ -1,3 +1,10 @@
-import string
-import datrie
-trie = datrie.Trie(string.ascii_lowercase)
+import marisa_trie
+# trie = marisa_trie.Trie([u"我", u'北京', u'天安门',"key1"])
+trie = marisa_trie.Trie(['key1', 'key2', 'key12','我'])
+
+trie.prefixes(u'我')
+trie.prefixes('key12')
+while True:
+    print("input text")
+    text = input()
+    print(trie.prefixes(text))
